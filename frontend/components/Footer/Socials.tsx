789d1d306dd2a9e1befb1e279cef61e43a0012eb
BipinKoirala7 <bipinkoirala2061@gmail.com>
2026-06-15 17:14:19 +0545

    Social Icons changed

--- frontend/components/Footer/Socials.tsx
+++ frontend/components/Footer/Socials.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import ExternalLink from "@/components/ui/ExternalLink";
-import { Link } from "lucide-react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaX,
+} from "react-icons/fa6";
 
 function Socials() {
   return (
     <div className="flex gap-6">
       <ExternalLink link="https://x.com" className="hover:text-blue-400">
-        <Link className="text-[1.5rem] smooth-transition" />
+        <FaTwitter className="text-[1.5rem] smooth-transition" />
       </ExternalLink>
       <ExternalLink
         link="https://instagram.com"
         className="hover:text-pink-500"
       >
-        <Link className="text-[1.5rem] smooth-transition" />
+        <FaInstagram className="text-[1.5rem] smooth-transition" />
       </ExternalLink>
       <ExternalLink link="https://facebook.com" className="hover:text-blue-400">
-        <Link className="text-[1.5rem]  smooth-transition" />
+        <FaFacebook className="text-[1.5rem]  smooth-transition" />
       </ExternalLink>
       <ExternalLink link="https://linkedin.com" className="hover:text-blue-400">
-        <Link className="text-[1.5rem] smooth-transition" />
+        <FaLinkedin className="text-[1.5rem] smooth-transition" />
       </ExternalLink>
     </div>
   );
